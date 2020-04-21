a1bc31cda10693708950d49fbb6504cf747a0386
Gregor Tzinov <gtzinov@gmail.com>
2020-04-20 18:45:56 -0600

    wip analysis

--- gtzinov_analysis.R
+++ gtzinov_analysis.R
@@ -13,21 +13,25 @@ data <- data %>%
   drop_na(Severity) %>%
   rename(Date = Start_Date)
 
-data <- data[complete.cases(data), ]
 data <- filter(data, Duration > 0)
+
+
 
 #Setting up for AR model: Variable of interest, number of severe accidents
 #with each day, where severe accident is defined by 'Severity > 3'
 AR_data <- filter(data, Severity > 2)
-ggplot(AR_data, x = Date, y = Severity)
 
 AR_data_collapsed <- group_by(AR_data, Date) %>% summarize(n()) %>%
   rename(c("count" = "n()"))
 
-ARdatatest = filter(AR_data_collapsed, Date > '2019-12-20')
-ggplot(data = AR_datatest, aes(x = Date, y = count)) + 
-  geom_line(color="blue", size = "2")
+AR_data_collapsed$Date <- as.Date(AR_data_collapsed$Date)
 
-ar.ols(AR_data_collapsed, aic= TRUE, na.action = na.fail)
+str(AR_data_collapsed)
+
+ggplot(data = AR_data_collapsed, aes(x= Date, y = count)) + 
+  geom_point()
+
+
+#ar.ols(AR_data_collapsed, aic= TRUE, na.action = na.fail)
 
 
